463fe63d82953c1042f93d279c8d15cd4c666133
hardik <hardik05@gmail.com>
2020-12-26 14:16:20 +0530

    added header check

--- imgRead.c
+++ imgRead.c
@@ -29,69 +29,76 @@ int ProcessImage(char* filename){
 
 	if(fp == NULL)
 	{
-		printf("\nCan't open file or file doesn't exist.");
+		printf("\nCan't open file or file doesn't exist.\r\n");
 		exit(0);
 	}
 
-	printf("\n\tHeader\twidth\theight\tdata\t\r\n");
 
-	while(fread(&img,sizeof(img),1,fp)>0){
-		printf("\n\t%s\t%d\t%d\t%s\r\n",img.header,img.width,img.height,img.data);
+	while(fread(&img,sizeof(img),1,fp)>0)
+	{
+		if(strcmp(img.header,"IMG")==0)
+		{
+			printf("\n\tHeader\twidth\theight\tdata\t\r\n");
 
-		//integer overflow 0x7FFFFFFF+1=0
-		//0x7FFFFFFF+2 = 1
-		//will cause very large/small memory allocation.
-		int size1 = img.width + img.height;
-		char* buff1=(char*)malloc(size1);
+			printf("\n\t%s\t%d\t%d\t%s\r\n",img.header,img.width,img.height,img.data);
 
-		//heap buffer overflow
-		memcpy(buff1,img.data,sizeof(img.data));
-		free(buff1);
-		//double free	
-		if (size1/2==0){
+
+			//integer overflow 0x7FFFFFFF+1=0
+			//0x7FFFFFFF+2 = 1
+			//will cause very large/small memory allocation.
+			int size1 = img.width + img.height;
+			char* buff1=(char*)malloc(size1);
+
+			//heap buffer overflow
+			memcpy(buff1,img.data,sizeof(img.data));
 			free(buff1);
-		}
-		else{
-			//use after free
-			if(size1 == 123456){
-				buff1[0]='a';
+			//double free	
+			if (size1/2==0){
+				free(buff1);
 			}
+			else{
+				//use after free
+				if(size1 == 123456){
+					buff1[0]='a';
+				}
+			}
+
+
+			//integer underflow 0-1=-1
+			//negative so will cause very large memory allocation
+			int size2 = img.width - img.height+100;
+			//printf("Size1:%d",size1);
+			char* buff2=(char*)malloc(size2);
+
+			//heap buffer overflow
+			memcpy(buff2,img.data,sizeof(img.data));
+
+			//divide by zero
+			int size3= img.width/img.height;
+			//printf("Size2:%d",size3);
+
+			char buff3[10];
+			char* buff4 =(char*)malloc(size3);
+			memcpy(buff4,img.data,sizeof(img.data));
+
+			//OOBR read bytes past buffer
+			char OOBR = buff3[size3];
+
+			//OOBW write bytes past buffer
+			buff3[size3]='c';
+
+			if(size3>10){
+				//memory leak here
+				buff4=0;
+			}
+			else{
+				free(buff4);
+			}
+
+			free(buff2);
 		}
-
-
-		//integer underflow 0-1=-1
-		//negative so will cause very large memory allocation
-		int size2 = img.width - img.height+100;
-		//printf("Size1:%d",size1);
-		char* buff2=(char*)malloc(size2);
-
-		//heap buffer overflow
-		memcpy(buff2,img.data,sizeof(img.data));
-
-		//divide by zero
-		int size3= img.width/img.height;
-		//printf("Size2:%d",size3);
-
-		char buff3[10];
-		char* buff4 =(char*)malloc(size3);
-		memcpy(buff4,img.data,sizeof(img.data));
-
-		//OOBR read bytes past buffer
-		char OOBR = buff3[size3];
-
-		//OOBW write bytes past buffer
-		buff3[size3]='c';
-
-		if(size3>10){
-			//memory leak here
-			buff4=0;
-		}
-		else{
-			free(buff4);
-		}
-
-		free(buff2);
-
+		else
+			printf("invalid header\r\n");
 
 	}
 	fclose(fp);
